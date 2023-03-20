e11b7183c820e47218997bef2ff66f93f20dfef0
jaishreegullani <jaishree.gullani@47billion.com>
2023-03-20 19:02:11 +0530

    Changes to Text Extraction

--- text_extractor/service.py
+++ text_extractor/service.py
@@ -1,39 +1,9 @@
 import io
-import os
-
-import cv2
 import pytesseract
-from minio import S3Error
 import globals
 from connection.minio_client_connection import minioClient
-
-
-def text_extract_from_file(file_id: int, file_name: str):
-    no_of_images = no_of_objects_in_file(file_id)
-    for i in range(no_of_images):
-        img = get_img_from_file(file_name, file_id, i)
-        config = '-l eng --oem 1 --psm 3'
-        text = pytesseract.image_to_string(img, config=config)
-        text_bytes = bytes(text, 'utf-8')
-        minioClient.put_object(globals.bucket_name,
-                               f"{file_id}/pages/page{i}/page{i}.txt",
-                               io.BytesIO(text_bytes),
-                               len(text_bytes))
-        print(f"Page{i}-----Text Extraction Done ")
-
-
-def get_img_from_file(current_file_name: str, current_file_id: int, page_no: str):
-    current_file_name = current_file_name.replace(".pdf", "")
-    try:
-        print(f"{current_file_id}/pages/page{page_no}/page{page_no}.jpg")
-        img = minioClient.get_object(bucket_name=globals.bucket_name,
-                                     object_name=f"{current_file_id}/pages/page{page_no}/pages{page_no}.jpg")
-        contents = img.read()
-        with open(f"{os.getcwd()}/files/{current_file_name}_page_{page_no}.jpg", "wb") as f:
-            f.write(contents)
-    except S3Error as e:
-        print(e)
-    return cv2.imread(f"{os.getcwd()}/files/{current_file_name}_page_{page_no}.jpg")
+from minio import S3Error
+from PIL import Image
 
 
 def no_of_objects_in_file(current_file_id: int):
@@ -43,3 +13,36 @@ def no_of_objects_in_file(current_file_id: int):
     except S3Error as err:
         print(err)
     return file_count
+
+
+def get_img_from_file(current_file_id: int, page_no: int):
+    try:
+        img = minioClient.get_object(globals.bucket_name, f"{current_file_id}/pages/page{page_no}/pages{page_no}.png")
+    except S3Error as e:
+        print(e)
+    return img
+
+
+def clean_text(text: str):
+    cleaned_text = ""
+    for char in text.split('\n'):
+        if char != "":
+            cleaned_text += char + "\n"
+    return cleaned_text
+
+
+def text_extract_from_file(file_id: int, file_name: str):
+    print("Inside Splitter")
+    no_of_images = no_of_objects_in_file(file_id)
+    print("no_of_images: " + str(no_of_images))
+    for i in range(no_of_images):
+        img = get_img_from_file(file_id, i)
+        contents = img.read()
+        text = pytesseract.image_to_string(Image.open(io.BytesIO(contents)), lang='eng')
+        text = clean_text(text)
+        text_bytes = bytes(text, 'utf-8')
+        minioClient.put_object(globals.bucket_name,
+                               f"{file_id}/pages/page{i}/pages{i}.txt",
+                               io.BytesIO(text_bytes),
+                               len(text_bytes))
+        print(f"Page{i}-----Text Extraction Done ")
