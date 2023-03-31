d1d3362e492d906887f02eb09d7bce8e2f7c7095
Lovesh Kumrawat <lovesh.kumrawat@47billion.com>
2023-03-31 11:37:42 +0530

    Debug fixes: file upload service

--- file_upload/service.py
+++ file_upload/service.py
@@ -7,35 +7,53 @@ from file_upload.models import File
 import globals
 from connection.minio_client_connection import minioClient
 from supporting.logs import logger
-from fastapi import HTTPException,status
+from fastapi import HTTPException, status
 
 
 def upload_file_to_minio(data, filename:str):
-    found = minioClient.bucket_exists(globals.bucket_name)
-    if not found:
-        minioClient.make_bucket(globals.bucket_name)
+
+    bucket = minioClient.bucket_exists(globals.bucket_name)
+    if not bucket: minioClient.make_bucket(globals.bucket_name)
+    
     try:
         file_id:int = add_file_to_database(filename)
-        minioClient.put_object(bucket_name=globals.bucket_name, object_name=f"{file_id}/files/{filename}",
-                               data=io.BytesIO(data), length=len(data), content_type="pdf")
-        logger.info(f"Successfully uploaded to {globals.bucket_name}")
+        
+        minioClient.put_object(
+            bucket_name=globals.bucket_name,
+            object_name=f"{file_id}/files/{filename}",
+            data=io.BytesIO(data),
+            length=len(data),
+            content_type="pdf"
+        )
+        
+        logger.info(f'File uploaded with {file_id} file id')
         return file_id
+    
     except S3Error as e:
-        logger.error("Error: {}".format(e))
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail='error in uplaoding file to minio')
+        logger.error("error in uplaoding file to minio", exc_info=e)
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="error in uplaoding file to minio")
 
 
 
 def add_file_to_database(file_name:str):
+    
     try:
-        file_id = session.query(func.max(File.id)).scalar()
-        if not file_id:
-            file_id = 0
-        file = File(id=file_id + 1, file_name=file_name, file_path=f"{file_id + 1}/files/{file_name}",
-                    uploaded_time=datetime.now())
+        file_id = (session.query(func.max(File.id)).scalar() or 0) + 1
+        file = File(
+            id=file_id,
+            file_name=file_name,
+            file_path=f"{file_id}/files/{file_name}",
+            uploaded_time=datetime.now()
+        )
+        
         session.add(file)
         session.commit()
-        logger.info('file added in database')
-        return file_id + 1
-    except:
-        logger.error('error in adding file data to database')
+        logger.info("file details added in database")
+        return file_id
+    
+    except Exception as e:
+        logger.error("error in adding file details to 'File Upload' database", exc_info=e)
+        raise HTTPException(
+            status_code=status.HTTP_304_NOT_MODIFIED,
+            detail="error in adding file details to 'File Upload' database"
+        )
