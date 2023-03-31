c925f4b6c911e1147fa0c0aabc2dadb9377d7d05
Lovesh Kumrawat <lovesh.kumrawat@47billion.com>
2023-03-31 11:38:00 +0530

    Debug fixes: page splitter service

--- page_splitter/service.py
+++ page_splitter/service.py
@@ -12,51 +12,88 @@ from page_splitter.models import PageSplitter
 
 
 def convert_to_image(file_id: int, file_name: str):
-    try:
-        response = get_file_from_minio(file_id, file_name)
-        data_entry = session.query(PageSplitter).filter(PageSplitter.id == file_id).first()
-        doc = fitz.open(stream=response.data, filetype="pdf")
-        for page in doc:
-            pix = page.get_pixmap(matrix=fitz.Identity, dpi=None, colorspace=fitz.csRGB, clip=None, alpha=True,
-                                  annots=True)
+
+    response = get_file_from_minio(file_id, file_name)
+    data_entry = add_file_details_to_db(file_id, file_name)
+    
+    doc = fitz.open(stream=response.data, filetype="pdf")
+    
+    for page in doc:
+        
+        try:
+            pix = page.get_pixmap(
+                matrix=fitz.Identity,
+                dpi=None,
+                colorspace=fitz.csRGB,
+                clip=None,
+                alpha=False,
+                annots=True
+            )
+        
+        except Exception as e:
+            data_entry.status = 'unsuccessful'
+            data_entry.error = e
+            session.commit()
+
+            logger.error("error while pdf to image conversion", exc_info=e)
+            raise HTTPException(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, detail="error while pdf to image conversion")
+        
+        try:
             page_name = f"pages{page.number}.jpg"
             img_bytes = pix.tobytes()
-            minioClient.put_object(bucket_name=globals.bucket_name,
-                                   object_name=f"{file_id}/pages/page{page.number}/{page_name}",
-                                   data=io.BytesIO(img_bytes), length=len(img_bytes), content_type="img/png")
+
+            minioClient.put_object(
+                bucket_name=globals.bucket_name,
+                object_name=f"{file_id}/pages/page{page.number}/{page_name}",
+                data=io.BytesIO(img_bytes),
+                length=len(img_bytes),
+                content_type="img/png"
+            )
+
+        except Exception as e:
+            data_entry.status = 'unsuccessful'
+            data_entry.error = e
+            session.commit()
+
+            logger.error("error while image uploading", exc_info=e)
+            raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="error while image uploading")
 
         data_entry.submission_time = datetime.now()
         data_entry.status = 'successful'
         data_entry.error = 'NULL'
         session.commit()
-
-    except Exception as e:
-        print("Inside splitter")
-        print(e)
-        logger.error('error in converting to image')
-        session.query(PageSplitter).filter(PageSplitter.id == file_id).update(
-            {PageSplitter.error: e, PageSplitter.status: 'unsuccessful'})
-        session.commit()
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='error in converting to image')
+        
+        logger.info(f'File converted to image with {file_id} file id')
 
 
 def get_file_from_minio(file_id: int, file_name: str):
+    
     try:
-
         response = minioClient.get_object(
             bucket_name=globals.bucket_name,
             object_name=f"{file_id}/files/{file_name}"
         )
 
-        logger.info(f"Successfully downloaded  from {globals.bucket_name}'.")
+        logger.info(f"File data fetched with {file_id} file id")
+        return response
+    
+    except S3Error as e:
+        logger.error("error in fetching file data from minio", exc_info=e)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="error in fetching file data from minio")
+
+
+def add_file_details_to_db(file_id: int, file_name: str):
+
+    try:
         file = PageSplitter(id=file_id, file_name=file_name, fetch_time=datetime.now())
         session.add(file)
         session.commit()
-
-        return response
+        logger.info('file details added in database')
+        return file
 
     except S3Error as e:
-        print(e)
-        logger.error("Error: {}".format(e))
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail='error in fetching file from minio')
+        logger.error("error in adding file details to 'Page Splitter' database", exc_info=e)
+        raise HTTPException(
+            status_code=status.HTTP_304_NOT_MODIFIED,
+            detail="error in adding file details to 'Page Splitter' database"
+        )
